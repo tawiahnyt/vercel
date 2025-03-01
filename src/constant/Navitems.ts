661fe57c040619593f602588d13848819654ad77
tawiah <tawiahin4k@gmail.com>
2025-03-01 03:01:04 +0000

    :memo:

---
+++ src/constant/Navitems.ts
@@ -0,0 +1,185 @@
+/** @format */
+
+export const navItems = [
+  {
+    navTitle: "Products",
+    navContents: [
+      {
+        title: "Dx Services",
+        subNav: [
+          {
+            heading: "Previews",
+            subHeading: "Helping teams ship 6x faster",
+            icon: "",
+          },
+          {
+            heading: "AI",
+            subHeading: "Powering breakthroughs",
+            icon: "",
+          },
+        ],
+      },
+      {
+        title: "Managed Infrastructure",
+        subNav: [
+          {
+            heading: "Fluid compute",
+            subHeading: "Servers, in serverless form",
+            icon: "",
+          },
+          {
+            heading: "Rendering",
+            subHeading: "Fast, scalable, and reliable",
+            icon: "",
+          },
+          {
+            heading: "Observability",
+            subHeading: "Trace every step",
+            icon: "",
+          },
+          {
+            heading: "Security",
+            subHeading: "Scale without compromising",
+            icon: "",
+          },
+        ],
+      },
+      {
+        title: "Open Source",
+        subNav: [
+          {
+            heading: "Next.js",
+            subHeading: "The native Next.js platform",
+            icon: "",
+          },
+          {
+            heading: "Turborepo",
+            subHeading: "Speed with Enterprise scale",
+            icon: "",
+          },
+          {
+            heading: "ALSDK",
+            subHeading: "The Al Toolkit for TypeScript",
+            icon: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    navTitle: "Solutions",
+    navContents: [
+      {
+        title: "Use Cases",
+        subNav: [
+          {
+            heading: "Al Apps",
+            subHeading: "Deploy at the speed of Al",
+            icon: "",
+          },
+          {
+            heading: "Composable Commerce",
+            subHeading: "Power storefronts that convert",
+            icon: "",
+          },
+          {
+            heading: "Marketing Sites",
+            subHeading: "Launch campaigns fast",
+            icon: "",
+          },
+          {
+            heading: "Multi-tenant Platforms",
+            subHeading: "Scale apps with one codebase",
+            icon: "",
+          },
+          {
+            heading: "Web Apps",
+            subHeading: "Ship features, not infrastructure",
+            icon: "",
+          },
+        ],
+      },
+      {
+        title: "Users",
+        subNav: [
+          {
+            heading: "Platform Engineers",
+            subHeading: "Automate away repetition",
+            icon: "",
+          },
+          {
+            heading: "Design Engineers",
+            subHeading: "Deploy for every idea",
+            icon: "",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    navTitle: "Resources",
+    navContents: [
+      {
+        title: "Tools",
+        subNav: [
+          {
+            heading: "Resource Center",
+            subHeading: "Today's best practices",
+            icon: "",
+          },
+          {
+            heading: "Marketplace",
+            subHeading: "Extend and automate workflows",
+            icon: "",
+          },
+          {
+            heading: "Templates",
+            subHeading: "Jumpstart app development",
+            icon: "",
+          },
+          {
+            heading: "Guides",
+            subHeading: "Find help quickly",
+            icon: "",
+          },
+          {
+            heading: "Partner Finder",
+            subHeading: "Get help from solution partners",
+            icon: "",
+          },
+        ],
+      },
+      {
+        title: "Company",
+        subNav: [
+          {
+            heading: "Customers",
+            subHeading: "Trusted by the best teams",
+            icon: "",
+          },
+          {
+            heading: "Blog",
+            subHeading: "The latest posts and changes",
+            icon: "",
+          },
+          {
+            heading: "Changelog",
+            subHeading: "See what shipped",
+            icon: "",
+          },
+          {
+            heading: "Press",
+            subHeading: "Read the latest news",
+            icon: "",
+          },
+        ],
+      },
+    ],
+  },
+  "Enterprise",
+  "Docs",
+  "Pricing",
+];
+
+
+
